f4af8a4ad122d01aa085cc2a7ff047ab25a2dc92
Klypalskyi <klypalskyi@gmail.com>
2018-08-06 03:52:00 +0300

    almost mobile

--- gulpfile.js
+++ gulpfile.js
@@ -95,7 +95,7 @@ gulp.task('images', () =>
 );
 
 gulp.task('fonts', () =>
-  gulp.src('./src/fonts/**/*.{woff,woff2}').pipe(gulp.dest('./build/fonts')),
+  gulp.src('./src/fonts/**/*.{woff,woff2,ttf}').pipe(gulp.dest('./build/fonts')),
 );
 
 gulp.task('watch', () => {
